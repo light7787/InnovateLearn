40f015d67894d2365dd0e5ad2e04a33a94db6d21
light7787 <141263462+light7787@users.noreply.github.com>
2024-12-29 01:52:30 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -37,7 +37,7 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 4000;
-
+console.log(process.env.MONGO_URI);
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => {
