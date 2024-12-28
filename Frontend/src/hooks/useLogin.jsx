93c7d93092d33c7ac403658fb01a9d693f858670
light7787 <141263462+light7787@users.noreply.github.com>
2024-12-29 01:58:04 +0530

    Update useLogin.jsx

--- Frontend/src/hooks/useLogin.jsx
+++ Frontend/src/hooks/useLogin.jsx
@@ -14,7 +14,7 @@ export const useLogin = () => {
         setError(null);
 
         try {
-            const response = await fetch('/api/login', {
+            const response = await fetch('https://innovate-learn-v1ki.vercel.app/api/login', {
                 method: 'POST',credentials: 'include',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ email, password }),
