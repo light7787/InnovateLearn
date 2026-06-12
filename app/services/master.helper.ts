cab6908b0158db70fe59fae87dbdc27378884422
Chinmaya Behera <141263462+light7787@users.noreply.github.com>
2026-06-12 13:35:27 +0530

    Delete app directory

--- app/services/master.helper.ts
+++
@@ -1,6 +0,0 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-export const getMasters = async () => {
-  const data = await AsyncStorage.getItem('masters');
-  return data ? JSON.parse(data) : null;
-};
